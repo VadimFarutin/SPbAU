b3e2d25e7a02816153907f3fa258719e08539f49
VadimFarutin <vfarut@gmail.com>
2016-09-08 23:00:41 +0300

    changed names of variables

--- para-task-01/list_task.py
+++ para-task-01/list_task.py
@@ -3,11 +3,11 @@
 # Example input: [1, 2, 2, 3]
 # Example output: [1, 2, 3]
 def remove_adjacent(lst):
-    newLst = [lst[0]]
+    new_lst = [lst[0]]
     for i in range(1, len(lst)):
         if lst[i] != lst[i - 1]:
-            newLst.append(lst[i])
-    return newLst
+            new_lst.append(lst[i])
+    return new_lst
 
 
 # Merge two sorted lists in one sorted list in linear time
@@ -15,19 +15,19 @@ def remove_adjacent(lst):
 # Example input: [2, 4, 6], [1, 3, 5]
 # Example output: [1, 2, 3, 4, 5, 6]
 def linear_merge(lst1, lst2):
-    newLst = []
+    new_lst = []
     i = 0
     j = 0
-    maxElem = max(lst1[-1], lst2[-1]) + 1
-    lst1.append(maxElem)
-    lst2.append(maxElem)
+    max_elem = max(lst1[-1], lst2[-1]) + 1
+    lst1.append(max_elem)
+    lst2.append(max_elem)
     len1 = len(lst1)
     len2 = len(lst2)
     for k in range(len1 + len2 - 2):
         if (lst1[i] < lst2[j]):
-            newLst.append(lst1[i])
+            new_lst.append(lst1[i])
             i += 1
         else:
-            newLst.append(lst2[j])
+            new_lst.append(lst2[j])
             j += 1
-    return newLst
+    return new_lst
