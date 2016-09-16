2e92bd04b1ed2376efa56cc008bcc4d91c7cda33
VadimFarutin <vfarut@gmail.com>
2016-09-17 00:31:26 +0300

    changed output

--- homework2/duplicates_search.py
+++ homework2/duplicates_search.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import hashlib
-import stat
 
 
 def duplicate_search(directory_name):
@@ -20,11 +19,11 @@ def duplicate_search(directory_name):
         hash_dict = {}
         for root, _, files in os.walk(directory_name):
             for name in files:
-                path = os.path.join(root, name)
+                path = os.path.abspath(os.path.join(root, name))
                 if name[0] != '.' and name[0] != '~' and not os.path.islink(path):
                     hash = md5(path)
                     hash_dict[hash] = hash_dict.get(hash, [])
-                    hash_dict[hash].append(path.replace(directory_name, "")[1:])
+                    hash_dict[hash].append(path)
         return hash_dict
 
     def print_duplicates(hash_dict):
