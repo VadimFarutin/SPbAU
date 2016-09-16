c82a14079e194ec882aa463ee296d8163c9e538f
VadimFarutin <vfarut@gmail.com>
2016-09-16 16:03:05 +0300

    made md5(), changed the way to walk through directories

--- homework2/duplicates_search.py
+++ homework2/duplicates_search.py
@@ -6,24 +6,24 @@ import hashlib
 def duplicate_search(directory_name):
     hash_dict = {}
 
+    def md5(path):
+        digest = hashlib.md5()
+        with open(path, "rb") as file:
+            for line in file:
+                digest.update(line)
+        hash_dict[digest.hexdigest()] = hash_dict.get(digest.hexdigest(), [])
+        hash_dict[digest.hexdigest()].append(path.replace(directory_name, "")[1:])
+
     def make_hashes(directory_name):
-        for entry in os.scandir(directory_name):
-            if entry.is_dir():
-                make_hashes(entry.path)
-            elif entry.name[0] != '.' and entry.name[0] != '~':
-                file = open(entry.path, "rb")
-                digest = hashlib.md5()
-                digest.update(file.read())
-                hash_dict[digest.hexdigest()] = hash_dict.get(digest.hexdigest(), []) + [entry.path]
-                file.close()
+        for root, _, files in os.walk(directory_name):
+            for name in files:
+                if name[0] != '.' and name[0] != '~':
+                    md5(os.path.join(root, name))
 
     def print_duplicates():
         for file_list in hash_dict.values():
             if len(file_list) > 1:
-                duplicates = ""
-                for file in file_list:
-                    duplicates += file.replace(directory_name, "")[1:] + ":"
-                print(duplicates[:-1] + "\n")
+                print(":".join(file_list))
 
     make_hashes(directory_name)
     print_duplicates()
