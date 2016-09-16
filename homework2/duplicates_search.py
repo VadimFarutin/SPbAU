6eae8b4c07e2298d9b00520ee72bc922fd1e75c1
VadimFarutin <vfarut@gmail.com>
2016-09-16 23:26:41 +0300

    fixed reading bytes; works with symlinks

--- homework2/duplicates_search.py
+++ homework2/duplicates_search.py
@@ -1,32 +1,41 @@
 import sys
 import os
 import hashlib
+import stat
 
 
 def duplicate_search(directory_name):
-    hash_dict = {}
 
     def md5(path):
         digest = hashlib.md5()
         with open(path, "rb") as file:
-            for line in file:
-                digest.update(line)
-        hash_dict[digest.hexdigest()] = hash_dict.get(digest.hexdigest(), [])
-        hash_dict[digest.hexdigest()].append(path.replace(directory_name, "")[1:])
+            while True:
+                byte = file.read(1)
+                if not byte:
+                    break
+                digest.update(byte)
+        return digest.hexdigest()
+
+    def is_symlink(path):
+        return stat.S_ISLNK(os.stat(path, follow_symlinks=False).st_mode)
 
     def make_hashes(directory_name):
+        hash_dict = {}
         for root, _, files in os.walk(directory_name):
             for name in files:
-                if name[0] != '.' and name[0] != '~':
-                    md5(os.path.join(root, name))
+                path = os.path.join(root, name)
+                if name[0] != '.' and name[0] != '~' and not is_symlink(path):
+                    hash = md5(path)
+                    hash_dict[hash] = hash_dict.get(hash, [])
+                    hash_dict[hash].append(path.replace(directory_name, "")[1:])
+        return hash_dict
 
-    def print_duplicates():
+    def print_duplicates(hash_dict):
         for file_list in hash_dict.values():
             if len(file_list) > 1:
                 print(":".join(file_list))
 
-    make_hashes(directory_name)
-    print_duplicates()
+    print_duplicates(make_hashes(directory_name))
 
 
 def main():
