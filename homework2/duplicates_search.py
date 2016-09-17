4a65dc7cdd1c9abf1c677aa79861e1b123f22804
VadimFarutin <vfarut@gmail.com>
2016-09-17 10:14:51 +0300

    changed project structure

--- homework2/duplicates_search.py
+++ homework2/duplicates_search.py
@@ -3,35 +3,33 @@ import os
 import hashlib
 
 
-def duplicate_search(directory_name):
+def md5(path):
+    digest = hashlib.md5()
+    with open(path, "rb") as file:
+        while True:
+            byte = file.read(1)
+            if not byte:
+                break
+            digest.update(byte)
+    return digest.hexdigest()
 
-    def md5(path):
-        digest = hashlib.md5()
-        with open(path, "rb") as file:
-            while True:
-                byte = file.read(1)
-                if not byte:
-                    break
-                digest.update(byte)
-        return digest.hexdigest()
 
-    def make_hashes(directory_name):
-        hash_dict = {}
-        for root, _, files in os.walk(directory_name):
-            for name in files:
-                path = os.path.abspath(os.path.join(root, name))
-                if name[0] != '.' and name[0] != '~' and not os.path.islink(path):
-                    hash = md5(path)
-                    hash_dict[hash] = hash_dict.get(hash, [])
-                    hash_dict[hash].append(path)
-        return hash_dict
+def make_hashes(directory_name):
+    hash_dict = {}
+    for root, _, files in os.walk(directory_name):
+        for name in files:
+            path = os.path.abspath(os.path.join(root, name))
+            if name[0] != '.' and name[0] != '~' and not os.path.islink(path):
+                hash = md5(path)
+                hash_dict[hash] = hash_dict.get(hash, [])
+                hash_dict[hash].append(path)
+    return hash_dict
 
-    def print_duplicates(hash_dict):
-        for file_list in hash_dict.values():
-            if len(file_list) > 1:
-                print(":".join(file_list))
 
-    print_duplicates(make_hashes(directory_name))
+def print_duplicates(hash_dict):
+    for file_list in hash_dict.values():
+        if len(file_list) > 1:
+            print(":".join(file_list))
 
 
 def main():
@@ -39,8 +37,7 @@ def main():
         print('usage: ./duplicates_search.py directory_name')
         sys.exit(1)
 
-    directory_name = sys.argv[1]
-    duplicate_search(directory_name)
+    print_duplicates(make_hashes(sys.argv[1]))
 
 if __name__ == '__main__':
     main()
