c50b9906aa134442fd6f130ecc291e4c1c46e26b
VadimFarutin <vfarut@gmail.com>
2016-09-24 20:39:26 +0300

    does not create waste matrices

--- homework3/strassen.py
+++ homework3/strassen.py
@@ -1,55 +1,76 @@
 import numpy as np
 
 
-def read_matrix(side_size):
-    increased_size = 1
-    while increased_size < side_size:
-        increased_size *= 2
+def zero_matrix(side_size):
+    return np.zeros((side_size, side_size), dtype=np.int64)
 
-    matrix = np.zeros((increased_size, increased_size), dtype=np.int64)
+
+def read_square_matrix(destination_matrix, side_size):
     for i in range(side_size):
         line = input().split()
-        for j in range(len(line)):
-            matrix[i][j] = int(line[j])
-    return matrix
+        for j in range(side_size):
+            destination_matrix[i][j] = int(line[j])
 
 
-def print_matrix(matrix, side_size):
-    matrix = matrix[:side_size, :side_size].tolist()
-    for line in matrix:
+def print_matrix(matrix):
+    for line in matrix.tolist():
         print(" ".join(map(str, line)))
 
 
-def split_matrix(matrix, side_size):
-    half_size = side_size // 2
+def split_matrix(matrix):
+    half_size = matrix.shape[0] // 2
     return [[np.matrix(matrix[:half_size, :half_size]), np.matrix(matrix[:half_size, half_size:])],
             [np.matrix(matrix[half_size:, :half_size]), np.matrix(matrix[half_size:, half_size:])]]
 
 
-def multiply_matrices(matrix_a, matrix_b, side_size):
+def multiply_matrices(matrix_res, matrix_a, matrix_b):
+    side_size = matrix_a.shape[0]
     if side_size == 1:
-        return np.dot(matrix_a, matrix_b)
+        matrix_new = np.dot(matrix_a, matrix_b)
+    else:
+        a = split_matrix(matrix_a)
+        b = split_matrix(matrix_b)
+        half_size = side_size // 2
+        I = zero_matrix(half_size)
+        II = zero_matrix(half_size)
+        III = zero_matrix(half_size)
+        IV = zero_matrix(half_size)
+        V = zero_matrix(half_size)
+        VI = zero_matrix(half_size)
+        VII = zero_matrix(half_size)
+        multiply_matrices(I, a[0][0] + a[1][1], b[0][0] + b[1][1])
+        multiply_matrices(II, a[1][0] + a[1][1], b[0][0])
+        multiply_matrices(III, a[0][0], b[0][1] - b[1][1])
+        multiply_matrices(IV, a[1][1], b[1][0] - b[0][0])
+        multiply_matrices(V, a[0][0] + a[0][1], b[1][1])
+        multiply_matrices(VI, a[1][0] - a[0][0], b[0][0] + b[0][1])
+        multiply_matrices(VII, a[0][1] - a[1][1], b[1][0] + b[1][1])
 
-    a = split_matrix(matrix_a, side_size)
-    b = split_matrix(matrix_b, side_size)
-    half_size = side_size // 2
-    I = multiply_matrices(a[0][0] + a[1][1], b[0][0] + b[1][1], half_size)
-    II = multiply_matrices(a[1][0] + a[1][1], b[0][0], half_size)
-    III = multiply_matrices(a[0][0], b[0][1] - b[1][1], half_size)
-    IV = multiply_matrices(a[1][1], b[1][0] - b[0][0], half_size)
-    V = multiply_matrices(a[0][0] + a[0][1], b[1][1], half_size)
-    VI = multiply_matrices(a[1][0] - a[0][0], b[0][0] + b[0][1], half_size)
-    VII = multiply_matrices(a[0][1] - a[1][1], b[1][0] + b[1][1], half_size)
+        matrix_new = np.vstack((np.hstack((I + IV - V + VII, III + V)),
+                                np.hstack((II + IV, I + III - II + VI))))
 
-    return np.vstack((np.hstack((I + IV - V + VII, III + V)),
-                      np.hstack((II + IV, I + III - II + VI))))
+    for i in range(side_size):
+        for j in range(side_size):
+            matrix_res[i][j] = matrix_new[i][j]
+
+
+def strassen(side_size):
+    increased_size = 2 ** (side_size.bit_length() - 1)
+    if increased_size < side_size:
+        increased_size *= 2
+
+    matrix_a = zero_matrix(increased_size)
+    read_square_matrix(matrix_a, side_size)
+    matrix_b = zero_matrix(increased_size)
+    read_square_matrix(matrix_b, side_size)
+    matrix_res = zero_matrix(increased_size)
+
+    multiply_matrices(matrix_res, matrix_a, matrix_b)
+    print_matrix(matrix_res[:side_size, :side_size])
 
 
 def main():
-    n = int(input())
-    matrix_a = read_matrix(n)
-    matrix_b = read_matrix(n)
-    print_matrix(multiply_matrices(matrix_a, matrix_b, matrix_a.shape[0]), n)
+    strassen(int(input()))
 
 
 if __name__ == '__main__':
