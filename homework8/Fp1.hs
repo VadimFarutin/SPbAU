d3d3910ed9aca5ec9be4d796e9424a6d413b7c1a
VadimFarutin <vfarut@gmail.com>
2016-11-13 00:30:11 +0300

    removed unnecessary head' and tail' calls; changed concat'

--- homework8/Fp1.hs
+++ homework8/Fp1.hs
@@ -8,11 +8,11 @@ tail' (_:x) = x
 -- 3. take' возвращает первые n >= 0 элементов исходного списка (0,5)
 take' :: Int -> [a] -> [a]
 take' n [] = []
-take' n xs = if n == 0 then [] else (head' xs):take' (n - 1) (tail' xs)
+take' n (x:xs) = if n == 0 then [] else x:take' (n - 1) xs
 -- 4. drop' возвращает список без первых n >= 0 элементов; если n больше длины -- списка, то пустой список. (0,5)
 drop' :: Int -> [a] -> [a]
 drop' n [] = []
-drop' n xs = if n == 0 then xs else drop' (n - 1) (tail' xs)
+drop' n (x:xs) = if n == 0 then (x:xs) else drop' (n - 1) xs
 -- 5. filter' возвращает список из элементов, для которых f возвращает True (0,5)
 filter' :: (a -> Bool) -> [a] -> [a]
 filter' f xs = [x | x <- xs, f x]
@@ -21,14 +21,16 @@ filter' f xs = [x | x <- xs, f x]
 -- foldl' (*) 4 [] == 4
 foldl' :: (a -> b -> a) -> a -> [b] -> a
 foldl' f z [] = z
-foldl' f z l = foldl' f (f z (head' l)) (tail' l)
+foldl' f z (x:l) = foldl' f (f z x) l
 -- 7. concat' принимает на вход два списка и возвращает их конкатенацию (0,5)
 -- concat' [1,2] [3] == [1,2,3]
 concat' :: [a] -> [a] -> [a]
-concat' xs ys = xs ++ ys
+concat' [] ys = ys
+concat' (x:xs) ys = x:concat' xs ys
 -- 8. quickSort' возвращает его отсортированный список (0,5)
 quickSort' :: Ord a => [a] -> [a]
 quickSort' [] = []
-quickSort' xs = concat' (concat' (quickSort' (filter' (\x -> x < (head' xs)) xs))
-                                 (filter' (\x -> x == (head' xs)) xs))
-                        (quickSort' (filter' (\x -> x > (head' xs)) xs))
+quickSort' xs = concat' (concat' (quickSort' (filter' (< h) xs))
+                                 (filter' (== h) xs))
+                        (quickSort' (filter' (> h) xs))
+				where h = head' xs
