2e4bbe3a2190531886b61cddf5e677ca2fe42ea0
VadimFarutin <vfarut@gmail.com>
2016-12-02 18:18:56 +0300

    4.sql made with left join

--- homework10/4.sql
+++ homework10/4.sql
@@ -1,7 +1,5 @@
-select Country.Name,
-       count(case when Country.Code = City.CountryCode and
-                       City.Population >= 1000000
-                  then 1 else null end) Millionaire
-       from Country, City
-group by Country.Name
+select First.Name, count(Second.Id) Millionaire
+       from Country First left join City Second
+       on First.Code = Second.CountryCode and Second.Population >= 1000000
+group by First.Name
 order by Millionaire desc;
