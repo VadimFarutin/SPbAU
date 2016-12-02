e2c44267b583ad0a2139bb445041e70d6b827b3c
VadimFarutin <vfarut@gmail.com>
2016-12-02 19:48:56 +0300

    7.sql fixed syntax error

--- homework10/7.sql
+++ homework10/7.sql
@@ -3,5 +3,5 @@ select First.Name
        on First.Code = Second.CountryCode
 where First.Population != 0
 group by First.Name
-having First.Population > 2 * sum(Second.Population) or count(Second.Id) == 0
+having First.Population > 2 * sum(Second.Population) or count(Second.Id) = 0
 order by First.Name;
