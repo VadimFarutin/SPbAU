f5d1d8d47cddd46df5da1b1f5d23aa2b18ad6251
VadimFarutin <vfarut@gmail.com>
2019-06-13 01:39:19 +0300

    Created IssueInfoPageElement

---
+++ tests/youtrack-test/src/test/java/ru/spbau/farutin/youtrack_test/elements/IssueInfoPageElement.java
@@ -0,0 +1,22 @@
+package ru.spbau.farutin.youtrack_test.elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class IssueInfoPageElement {
+    private TextPageElement summaryText;
+    private TextPageElement descriptionText;
+
+    public IssueInfoPageElement(WebDriver webDriver) {
+        summaryText = new TextPageElement(webDriver, By.id("id_l.I.ic.icr.it.issSum"));
+        descriptionText = new TextPageElement(webDriver, By.id("l.I.ic.icr.d.description"));
+    }
+
+    public String getSummary() {
+        return summaryText.read();
+    }
+
+    public String getDesciption() {
+        return descriptionText.read();
+    }
+}
