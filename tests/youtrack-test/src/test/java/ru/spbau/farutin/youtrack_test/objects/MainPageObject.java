82600221f4434a9a7cb9c7d1382eaebd7cccd69e
VadimFarutin <vfarut@gmail.com>
2019-06-13 01:29:36 +0300

    Created MainPageObject

---
+++ tests/youtrack-test/src/test/java/ru/spbau/farutin/youtrack_test/objects/MainPageObject.java
@@ -0,0 +1,23 @@
+package ru.spbau.farutin.youtrack_test.objects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import ru.spbau.farutin.youtrack_test.elements.ButtonPageElement;
+
+public class MainPageObject {
+    private static final String ISSUES_URL = "http://localhost:8080/dashboard";
+
+    private WebDriver webDriver;
+    private ButtonPageElement createIssueButton;
+
+    public MainPageObject(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        this.webDriver.get(ISSUES_URL);
+        createIssueButton = new ButtonPageElement(webDriver, By.xpath("/html/body/div[1]/div[1]/div/a[5]"));
+    }
+
+    public CreateIssuePageObject createIssue() {
+        createIssueButton.click();
+        return new CreateIssuePageObject(webDriver);
+    }
+}
