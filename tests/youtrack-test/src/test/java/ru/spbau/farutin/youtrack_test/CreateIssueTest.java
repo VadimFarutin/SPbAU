56ea089baade625a2e35eff6fc0919256787d944
VadimFarutin <vfarut@gmail.com>
2019-06-13 14:26:09 +0300

    Added more tests

--- tests/youtrack-test/src/test/java/ru/spbau/farutin/youtrack_test/CreateIssueTest.java
+++ tests/youtrack-test/src/test/java/ru/spbau/farutin/youtrack_test/CreateIssueTest.java
@@ -45,7 +45,128 @@ public class CreateIssueTest {
 
     @Test
     public void testSimple() {
-        testSuccess("summary", "description");
+        testSuccess("summary", "description", "summary", "description");
+    }
+
+    @Test
+    public void testSummaryWithSpaces() {
+        testSuccess("summary   summary", "description", "summary summary", "description");
+    }
+
+    @Test
+    public void testSummaryWithFirstSpace() {
+        testSuccess(" summary", "description", "summary", "description");
+    }
+
+    @Test
+    public void testSummaryWithLastSpace() {
+        testSuccess("summary ", "description", "summary", "description");
+    }
+
+    @Test
+    public void testDescriptionWithSpaces() {
+        testSuccess("summary", "description   description", "summary", "description   description");
+    }
+
+    @Test
+    public void testDescriptionWithFirstSpace() {
+        testSuccess("summary", " description", "summary", " description");
+    }
+
+    @Test
+    public void testDescriptionWithLastSpace() {
+        testSuccess("summary", "description ", "summary", "description ");
+    }
+
+    @Test
+    public void testSummaryWithCapitalLetters() {
+        testSuccess("SumMarY", "description", "SumMarY", "description");
+    }
+
+    @Test
+    public void testDescriptionWithCapitalLetters() {
+        testSuccess("summary", "DeScriPtioN", "summary", "DeScriPtioN");
+    }
+
+    @Test
+    public void testSummaryWithDigits() {
+        testSuccess("123hello", "description", "123hello", "description");
+    }
+
+    @Test
+    public void testDescriptionWithDigits() {
+        testSuccess("summary", "123hello", "summary", "123hello");
+    }
+
+    @Test
+    public void testSummaryWithSpecialCharacters() {
+        testSuccess("`~!@#$%^&*()_+-=[]{}:;\'\"\\|,<.>/?", "description", "`~!@#$%^&*()_+-=[]{}:;\'\"\\|,<.>/?", "description");
+    }
+
+    @Test
+    public void testDescriptionWithSpecialCharacters() {
+        testSuccess("summary", "`~!@#$%^&*()_+-=[]{}:;\'\"\\|,<.>/?", "summary", "`~!@#$%^&*()_+-=[]{}:;\'\"\\|,<.>/?");
+    }
+
+    @Test
+    public void testLongSummary() {
+        String c = "a";
+        String summary = new String(new char[100]).replace("\0", c);
+        testSuccess(summary, "description", summary, "description");
+    }
+
+    @Test
+    public void testLongDescription() {
+        String c = "a";
+        String description = new String(new char[100]).replace("\0", c);
+        testSuccess("summary", description, "summary", description);
+    }
+
+    @Test
+    public void testExistedSummary() {
+        testSuccess("summary", "description", "summary", "description");
+        testSuccess("summary", "description1", "summary", "description1");
+    }
+
+    @Test
+    public void testExistedDescription() {
+        testSuccess("summary", "description", "summary", "description");
+        testSuccess("summary1", "description", "summary1", "description");
+    }
+
+    @Test
+    public void testSummaryWithRussianLetters() {
+        testSuccess("название", "description", "название", "description");
+    }
+
+    @Test
+    public void testDescriptionWithRussianLetters() {
+        testSuccess("summary", "описание", "summary", "описание");
+    }
+
+    @Test
+    public void testSummaryWithUnicodeCharacters() {
+        testSuccess("∞€£¥©®™αβӦ\u0558ڸ", "description", "∞€£¥©®™αβӦ\u0558ڸ", "description");
+    }
+
+    @Test
+    public void testDescriptionWithUnicodeCharacters() {
+        testSuccess("summary", "∞€£¥©®™αβӦ\u0558ڸ", "summary", "∞€£¥©®™αβӦ\u0558ڸ");
+    }
+
+    @Test
+    public void testSummaryWithNewLine() {
+        testSuccess("a\nb", "description", "ab", "description");
+    }
+
+    @Test
+    public void testDescriptionWithNewLine() {
+        testSuccess("summary", "a\nb", "summary", "a\nb");
+    }
+
+    @Test
+    public void testEmptyDescription() {
+        testSuccess("summary", "", "summary", "No description");
     }
 
     @Test
@@ -73,12 +194,12 @@ public class CreateIssueTest {
         createIssuePage.clickCreateIssue();
     }
 
-    private void testSuccess(String summary, String description) {
+    private void testSuccess(String summary, String description, String expectedSummary, String expectedDescription) {
         createIssue(summary, description);
 
         IssueInfoPageElement issueInfo = new IssueInfoPageElement(webDriver);
-        assertEquals(summary, issueInfo.getSummary());
-        assertEquals(description, issueInfo.getDesciption());
+        assertEquals(expectedSummary, issueInfo.getSummary());
+        assertEquals(expectedDescription, issueInfo.getDesciption());
     }
 
     private void testFail(String summary, String description, String reason) {
